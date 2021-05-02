1ce2cd7e48c1d61338e5ba108dc6cd18d93a4ea2
FelipheGomez <feliphegomez@gmail.com>
2021-05-02 12:49:12 -0500

    Add files system

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @package    PACMEC
+ * @category   System
+ * @copyright  2020-2021 Manager Technology CO & FelipheGomez CO
+ * @author     FelipheGomez <feliphegomez@gmail.com>
+ * @license    license.txt
+ * @version    0.0.1
+ */
+
+require __DIR__ . '/.pacmec/run.php';
